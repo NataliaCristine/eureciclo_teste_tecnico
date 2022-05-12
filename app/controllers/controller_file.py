23584f102b0a1870c9175774ac99542c2d7f8b7d
Natalia Nunes <naticristinenunes@gmail.com>
2022-05-12 10:32:25 -0300

    feature parse dados e config sqlalchemy

--- app/controllers/controller_file.py
+++ app/controllers/controller_file.py
@@ -1,12 +1,21 @@
 from flask import Flask
 import os
 from uuid import uuid4
+import csv
 
 def file_upload_save(data):
-    
+
     paths = ''
+    dados_list=[]
     
     for key,item in data.items():
         extension = item.filename.split('.')[1]
         paths= f'app/file/ar{uuid4()}.{extension}'
         item.save(paths)
+
+        with open(paths) as file_data:
+            data_reade = csv.DictReader(file_data,delimiter='\t')
+            for line in data_reade:
+                dados_list.append(dict(line))
+    
+
